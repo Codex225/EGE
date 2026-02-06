8cf2562b57146f075084a25a6fb5f088b2100cdd
Codex225 <hohlov19@mail.ru>
2026-02-06 19:54:12 +0300

    push

--- script1.py
+++ script1.py
@@ -1,9 +1,8 @@
-import sys
 
-velikans = set()
 
-for i in sys.stdin.readlines():
-    name, items = i.split()[0], i.split()[1:]
-    if 'gold' in items and 'silver' in items:
-        velikans.add(name)
-print('; '.join(velikans))
+mz = 0
+for x in range(0, 11501):
+    a = 7*270 + 7*170 + 7*70 - x
+    a7 = int(str(a), 7)
+
+
