654f1c9917cbc7917086d346c62b85fc04d10cf9
khokh <hohlov19@mail.ru>
2026-03-24 23:05:24 +0300

    ф

---
+++ 26/Хронология и реализация/8512.py
@@ -0,0 +1,19 @@
+f = open("26_8512.txt")
+k = int(f.readline())
+n = f.readline()
+data = []
+cells = [-1] * k
+for s in f:
+    start, end = [int(i) for i in s.split()]
+    data.append([start, end])
+last = -1
+data.sort()
+count = 0
+for passenger in data:
+    for i in range(len(cells)):
+        if passenger[0] > cells[i]:
+            cells[i] = passenger[1]
+            count += 1
+            last = i
+            break
+print(count, last + 1)
---
+++ 26/Хронология и реализация/9793.py
@@ -0,0 +1,31 @@
+f = open("26_9793.txt")
+
+n = int(f.readline())
+data = []
+num = 0
+for s in f:
+    shlif, okr = [int(x) for x in s.split()]
+    num += 1
+    if shlif < okr:
+        data.append([shlif, "s", num])
+    else:
+        data.append([okr, "o", num])
+data.sort()
+lenta = [0] * n
+k1 = 0
+k2 = -1
+last = 0
+for x in data:
+    if x[1] == "s":
+        lenta[k1] = x[2]
+        k1 += 1
+
+    else:
+        lenta[k2] = x[2]
+        k2 -= 1
+    last = x
+print(last[2])
+if last[1] == "s":
+    print(k1 - 1)
+else:
+    print(k1)
