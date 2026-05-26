357980974c852567cf2f4b92bce7f3063ba66594
khokh <hohlov19@mail.ru>
2026-05-26 15:08:11 +0300

    ф

---
+++ 24/Система задач 1-6/24-d1.py
@@ -0,0 +1,33 @@
+import time
+
+start_time = time.time()
+
+# line = open("24-d1.txt").readline()
+# maxlen = 0
+# s = ""
+# for i in range(len(line)):
+#
+#     if line[i] in "ACGK":
+#         s = s + line[i]
+#     else:
+#         maxlen = max(maxlen, len(s))
+#         s = ""
+# print(maxlen)
+# end_time = time.time()
+# execution_time = end_time - start_time
+# print(f"Время выполнения: {execution_time:.6f} секунд")
+
+s = open('24-d1.txt').readline().strip()
+
+k = m = 0
+for i in range(len(s)):
+  if s[i] in 'ACGK':
+    k += 1
+    m = max(m,k)
+  else:
+    k = 0
+print(m)
+
+end_time = time.time()
+execution_time = end_time - start_time
+print(f"Время выполнения: {execution_time:.6f} секунд")
